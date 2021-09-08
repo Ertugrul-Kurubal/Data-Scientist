fb25167e478ad8bce8a4383431a4e9701e19fb96
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-09-08 22:02:09 +0300

    flask py

--- Capstone Project/Fraud Detection/flask.py
+++ Capstone Project/Fraud Detection/flask.py
@@ -3,49 +3,49 @@ import pandas as pd
 import joblib
 import pickle
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route("/api", methods = ["GET", "POST"])
-def home():
-    if request.method == "POST":
-        my_dict = {
-            "time": request.form["time"],
-            "v1": request.form["v1"],
-            "v2": request.form["v2"],
-            "v3": request.form["v3"],
-            "v4": request.form["v4"],
-            "v5": request.form["v5"],
-            "v6": request.form["v6"],
-            "v7": request.form["v7"],
-            "v8": request.form["v8"],
-            "v9": request.form["v9"],
-            "v10": request.form["v10"],
-            "v11": request.form["v11"],
-            "v12": request.form["v12"],
-            "v13": request.form["v13"],
-            "v14": request.form["v14"],
-            "v15": request.form["v15"],
-            "v16": request.form["v16"],
-            "v17": request.form["v17"],
-            "v18": request.form["v18"],
-            "v19": request.form["v19"],
-            "v20": request.form["v20"],
-            "v21": request.form["v21"],
-            "v22": request.form["v22"],
-            "v23": request.form["v23"],
-            "v24": request.form["v24"],
-            "v25": request.form["v25"],
-            "v26": request.form["v26"],
-            "v27": request.form["v27"],
-            "v28": request.form["v28"],
-            "amount": request.form["amount"]
-            }
-        my_dict = pd.DataFrame([my_dict], columns=loaded_columns)
-        my_dict = loaded_scaler.transform(my_dict)
-        result = loaded_model.predict(my_dict)
-        return {"predicted price:":str(result)}
-    else:
-        return "get method"
+# @app.route("/api", methods = ["GET", "POST"])
+# def home():
+#     if request.method == "POST":
+#         my_dict = {
+#             "time": request.form["time"],
+#             "v1": request.form["v1"],
+#             "v2": request.form["v2"],
+#             "v3": request.form["v3"],
+#             "v4": request.form["v4"],
+#             "v5": request.form["v5"],
+#             "v6": request.form["v6"],
+#             "v7": request.form["v7"],
+#             "v8": request.form["v8"],
+#             "v9": request.form["v9"],
+#             "v10": request.form["v10"],
+#             "v11": request.form["v11"],
+#             "v12": request.form["v12"],
+#             "v13": request.form["v13"],
+#             "v14": request.form["v14"],
+#             "v15": request.form["v15"],
+#             "v16": request.form["v16"],
+#             "v17": request.form["v17"],
+#             "v18": request.form["v18"],
+#             "v19": request.form["v19"],
+#             "v20": request.form["v20"],
+#             "v21": request.form["v21"],
+#             "v22": request.form["v22"],
+#             "v23": request.form["v23"],
+#             "v24": request.form["v24"],
+#             "v25": request.form["v25"],
+#             "v26": request.form["v26"],
+#             "v27": request.form["v27"],
+#             "v28": request.form["v28"],
+#             "amount": request.form["amount"]
+#             }
+#         my_dict = pd.DataFrame([my_dict], columns=loaded_columns)
+#         my_dict = loaded_scaler.transform(my_dict)
+#         result = loaded_model.predict(my_dict)
+#         return {"predicted price:":str(result)}
+#     else:
+#         return "get method"
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
